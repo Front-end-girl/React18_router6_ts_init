9ccc6e09c99554b80c72bc168f646f3f6bef4ab7
lyn <luoyunna@newspiral.net>
2022-05-25 14:40:08 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    endOfLine: 'auto',
+    printWidth: 120,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: false,
+    bracketSameLine: true,
+    arrowParens: 'avoid',
+    insertPragma: false,
+    tabWidth: 4,
+    useTabs: false,
+}
